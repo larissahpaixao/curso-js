ac9518539c6c32528e727d096c84b55dc30e06d7
Larissa Paixão <larissa.hpaixao@gmail.com>
2025-03-03 10:01:26 -0300

    ex018

---
+++ aula16ex/ex018/modelo/script.js
@@ -0,0 +1,30 @@
+var num = window.document.getElementById('fnum')
+var lista = window.document.getElementById('flista')
+var res = window.document.getElementById('res')
+var valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1 ) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function adicionar () {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
